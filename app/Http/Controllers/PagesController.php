1e98c1dc8f397e3fa5a3ffeae16ed8c8e4993796
Bawik Ardiyan Ramadhan <ardiyanramadhan4@gmail.com>
2023-04-19 16:23:24 +0700

    19-04-2023

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -49,154 +49,216 @@ class PagesController extends Controller
 
     private function perhitungan_diagnosa($data)
     {
-        $data_penyakit = [];
-        $gejala_dipilih = [];
-        foreach($data['bobot_gejala'] as $value) {
-            if(!empty($value)) {
-                $opts = explode('+', $value);
-                $data_gejala[] = $opts[0];
-                $gejala = DB::table('knowladges')->where('kd_gejala' ,$opts[0])->get();
+        // dd(count($data['P01']) >= 3 && count($data['P02']) >= 3);
+        // $data_penyakit = [];
+        // $gejala_dipilih = [];
+        // foreach($data['bobot_gejala'] as $value) {
+        //     if(!empty($value)) {
+        //         $opts = explode('+', $value);
+        //         $data_gejala[] = $opts[0];
+        //         $gejala = DB::table('knowladges')->where('kd_gejala' ,$opts[0])->get();
 
-                foreach($gejala as $valuegejala) {
-                    if(empty($data_penyakit[$valuegejala->kd_diagnosa])) {
-                        $data_penyakit[$valuegejala->kd_diagnosa] = [$valuegejala, [$valuegejala, $opts[1], $valuegejala->nilai_mb, $valuegejala->kd_diagnosa, $valuegejala->kd_gejala]];
-                    } else {
-                        array_push($data_penyakit[$valuegejala->kd_diagnosa], [$valuegejala, $opts[1], $valuegejala->nilai_mb, $valuegejala->kd_diagnosa, $valuegejala->kd_gejala]);
+        //         foreach($gejala as $valuegejala) {
+        //             if(empty($data_penyakit[$valuegejala->kd_diagnosa])) {
+        //                 $data_penyakit[$valuegejala->kd_diagnosa] = [$valuegejala, [$valuegejala, $opts[1], $valuegejala->nilai_mb, $valuegejala->kd_diagnosa, $valuegejala->kd_gejala]];
+        //             } else {
+        //                 array_push($data_penyakit[$valuegejala->kd_diagnosa], [$valuegejala, $opts[1], $valuegejala->nilai_mb, $valuegejala->kd_diagnosa, $valuegejala->kd_gejala]);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // dd(count($data_penyakit['P02']) < 3);die;
+        // dd(count($data_penyakit['P02']));die;
+            $cf_value = null;
+            // $p01 = 0;
+            // $p02 = 0;
+            // $i = 0;
+            if(count($data['P01']) >= 3 && count($data['P02']) >= 3) {
+                foreach($data as $final) {
+                    // dd($final);die;
+                    if(count($final) < 3) {
+                        continue;
                     }
-                }
-            }
-        }
-
-        // dd($data_penyakit);die;
-        // if(count($data_penyakit['P01']) >= 2 || count($data_penyakit[''])
-
-        $hasil_diagnosa = [];
-        $cf_value = null;
-        // $p01 = 0;
-        // $p02 = 0;
-        foreach($data_penyakit as $final) {
-            // dd($final);die;
-            if(count($final) < 3) {
-                continue;
-            }
-
-            $cf1 = null;
-            $cf2 = null;
-            $cf_combine = 0;
-            $hasil_cf = null;
-            foreach($final as $key => $value) {
-                // if($key > 0) {
-                //     $data_test[] = $final[$key][3];
-                // }
-                if($key == 0) {
-                    continue;
-                } else {
-                    if($cf_combine != 0) {
-                        $cf1 = $cf_combine ;
-                        $cf2 = $final[$key][2] * $final[$key][1];
-
-                        if($cf1 < 0 || $cf2 < 0) {
-                            $cf_combine = ($cf1 + $cf2) / (1 - min($cf1, $cf2));
-                        } else {
-                            $cf_combine = $cf1 + ($cf2 * (1 - $cf1));
-                        }
-
-                        $hasil_cf = $cf_combine;
-                    } else {
-                        $cf2  = $final[$key][2] * $final[$key][1];
-
-                        if($cf1 < 0 || $cf2 < 0) {
-                            $cf_combine = ($cf1 + $cf2) / (1 - min($cf1, $cf2));
-                        } else {
-                            $cf_combine = $cf1 + ($cf2 * (1 - $cf1));
-                        }
-
-                        $hasil_cf = $cf_combine;
-                    }
-                }
-
-                if(count($final) - 1 == $key) {
-                    if($cf_value == null) {
-                        $cf_value = [$hasil_cf, "{$final[0]->kd_diagnosa} ({$final[0]->kd_gejala})"];
-                    } else {
-                        $cf_value = ($hasil_cf > $cf_value[0])
-                            ? [$hasil_cf, "{$final[0]->kd_diagnosa} ({$final[0]->kd_gejala})"]
-                            : $cf_value;
-                    }
-
-                    $hasil_diagnosa[$final[0]->kd_diagnosa]['hasil_cf'] = $hasil_cf * 100;
-
                     $cf1 = null;
                     $cf2 = null;
                     $cf_combine = 0;
                     $hasil_cf = null;
+                    // dd($final);
+                    foreach($final as $key => $value) {
+                        // if($key > 0) {
+                        //     $data_test[] = $final[$key][3];
+                        // }
+                        if($key > 0) {
+                            if($cf_combine != 0) {
+                                $cf1 = $cf_combine ;
+                                $cf2 = $final[$key][2] * $final[$key][1];
+
+                                if($cf1 < 0 || $cf2 < 0) {
+                                    $cf_combine = ($cf1 + $cf2) / (1 - min($cf1, $cf2));
+                                } else {
+                                    $cf_combine = $cf1 + ($cf2 * (1 - $cf1));
+                                }
+
+                                $hasil_cf = $cf_combine;
+                            } else {
+                                $cf2  = $final[$key][2] * $final[$key][1];
+
+                                if($cf1 < 0 || $cf2 < 0) {
+                                    $cf_combine = ($cf1 + $cf2) / (1 - min($cf1, $cf2));
+                                } else {
+                                    $cf_combine = $cf1 + ($cf2 * (1 - $cf1));
+                                }
+
+                                $hasil_cf = $cf_combine;
+                            }
+                        }
+
+                        if(count($final) - 1 == $key) {
+                            if($cf_value == null) {
+                                $cf_value = [$hasil_cf, "{$final[0]->kd_diagnosa} ({$final[0]->kd_gejala})"];
+                            } else {
+                                $cf_value = ($hasil_cf > $cf_value[0])
+                                    ? [$hasil_cf, "{$final[0]->kd_diagnosa} ({$final[0]->kd_gejala})"]
+                                    : $cf_value;
+                            }
+
+                            $hasil_diagnosa[$final[0]->kd_diagnosa]['hasil_cf'] = $hasil_cf * 100;
+
+                            $cf1 = null;
+                            $cf2 = null;
+                            $cf_combine = 0;
+                            $hasil_cf = null;
+                        }
+
+                        if(empty($hasil_diagnosa[$final[0]->kd_diagnosa])) {
+                                $hasil_diagnosa[$final[0]->kd_diagnosa] = [
+                                    // 'nama_penyakit' => $final[0]->kd_penyakit,
+                                    'kode_penyakit' => $final[0]->kd_diagnosa,
+                                    'hasil_cf' => $hasil_cf * 100
+                                ];
+                        }
+                    }
                 }
-
-                // if($key > 0) {
-
-                //     if($final[$key][3] == 'P01') {
-                //         $p01 += 1;
-                //     } else {
-                //         $p02 += 1;
-                //     }
-
-                //     if($p01 > $p02) {
-                //         $kd_diagnosa = 'P01';
-                //     } else if ($p01 == $p02) {
-                //         $kd_diagnosa = 'P01';
-                //     } else {
-                //         $kd_diagnosa = 'P02';
-                //     }
-
-                // }
-
-                if(empty($hasil_diagnosa[$final[0]->kd_diagnosa])) {
-                        $hasil_diagnosa[$final[0]->kd_diagnosa] = [
-                            // 'nama_penyakit' => $final[0]->kd_penyakit,
-                            'kode_penyakit' => $final[0]->kd_diagnosa,
-                            'hasil_cf' => $hasil_cf * 100
-                        ];
+                // dd($hasil_diagnosa);
+                if(count($hasil_diagnosa) > 1) {
+                    if($hasil_diagnosa['P01']['hasil_cf'] > $hasil_diagnosa['P02']['hasil_cf']) {
+                        $data_diagnosa = $hasil_diagnosa['P01']['kode_penyakit'];
+                        $nilai_hasil = $hasil_diagnosa['P01']['hasil_cf'];
+                        $data_diagnosa2 = $hasil_diagnosa['P02']['kode_penyakit'];
+                        $nilai_hasil2 = $hasil_diagnosa['P02']['hasil_cf'];
+                    }
+                    else if($hasil_diagnosa['P02']['hasil_cf'] > $hasil_diagnosa['P01']['hasil_cf']) {
+                        $data_diagnosa = $hasil_diagnosa['P02']['kode_penyakit'];
+                        $nilai_hasil = $hasil_diagnosa['P02']['hasil_cf'];
+                        $data_diagnosa2 = $hasil_diagnosa['P01']['kode_penyakit'];
+                        $nilai_hasil2 = $hasil_diagnosa['P01']['hasil_cf'];
+                    }
+                    else if($hasil_diagnosa['P01']['hasil_cf'] = $hasil_diagnosa['P02']['hasil_cf']) {
+                        $data_diagnosa = $hasil_diagnosa['P01']['kode_penyakit'];
+                        $nilai_hasil = $hasil_diagnosa['P01']['hasil_cf'];
+                        $data_diagnosa2 = $hasil_diagnosa['P02']['kode_penyakit'];
+                        $nilai_hasil2 = $hasil_diagnosa['P02']['hasil_cf'];
+                    }
                 }
+            }
+            else {
+                foreach($data as $final) {
+                    // dd($final);die;
+                    if(count($final) >= 3) {
+                        // $nilaiiiiii[] = $final;
+                        $cf1 = null;
+                        $cf2 = null;
+                        $cf_combine = 0;
+                        $hasil_cf = null;
+                        $hasil_diagnosa = [];
+                        // dd($final);
+                        foreach($final as $key => $value) {
+                            // if($key > 0) {
+                            //     $data_test[] = $final[$key][3];
+                            // }
+                            if($key > 0) {
+                                if($cf_combine != 0) {
+                                    $cf1 = $cf_combine ;
+                                    $cf2 = $final[$key][2] * $final[$key][1];
 
-            }
-        }
-        if(count($hasil_diagnosa) == 1) {
-            if($hasil_diagnosa) {
-                $data_diagnosa = $hasil_diagnosa[$final[0]->kd_diagnosa]['kode_penyakit'];
-                $nilai_hasil = $hasil_diagnosa[$final[0]->kd_diagnosa]['hasil_cf'];
-                $data_diagnosa2 = '-';
-                $nilai_hasil2 = '0';
-            }
-        } else {
-            if($hasil_diagnosa['P01']['hasil_cf'] > $hasil_diagnosa['P02']['hasil_cf']) {
-                $data_diagnosa = $hasil_diagnosa['P01']['kode_penyakit'];
-                $nilai_hasil = $hasil_diagnosa['P01']['hasil_cf'];
-                $data_diagnosa2 = $hasil_diagnosa['P02']['kode_penyakit'];
-                $nilai_hasil2 = $hasil_diagnosa['P02']['hasil_cf'];
-            }
-            // else if($hasil_diagnosa['P01']['hasil_cf'] = $hasil_diagnosa['P02']['hasil_cf']) {
-            //     $data_diagnosa = $hasil_diagnosa['P01']['kode_penyakit'];
-            //     $nilai_hasil = $hasil_diagnosa['P01']['hasil_cf'];
-            //     $data_diagnosa2 = $hasil_diagnosa['P02']['kode_penyakit'];
-            //     $nilai_hasil2 = $hasil_diagnosa['P02']['hasil_cf'];
-            // }
-            else if($hasil_diagnosa['P02']['hasil_cf'] > $hasil_diagnosa['P01']['hasil_cf']) {
-                $data_diagnosa = $hasil_diagnosa['P02']['kode_penyakit'];
-                $nilai_hasil = $hasil_diagnosa['P02']['hasil_cf'];
-                $data_diagnosa2 = $hasil_diagnosa['P01']['kode_penyakit'];
-                $nilai_hasil2 = $hasil_diagnosa['P01']['hasil_cf'];
-            }
-        }
+                                    if($cf1 < 0 || $cf2 < 0) {
+                                        $cf_combine = ($cf1 + $cf2) / (1 - min($cf1, $cf2));
+                                    } else {
+                                        $cf_combine = $cf1 + ($cf2 * (1 - $cf1));
+                                    }
 
-        // dd([$data_diagnosa, $nilai_hasil, $data_diagnosa2, $nilai_hasil2]);die;
+                                    $hasil_cf = $cf_combine;
+                                } else {
+                                    $cf2  = $final[$key][2] * $final[$key][1];
 
-        return [
-            'data_diagnosa' => $data_diagnosa,
-            'nilai_hasil'   => $nilai_hasil,
-            'data_diagnosa2' => $data_diagnosa2,
-            'nilai_hasil2'   => $nilai_hasil2,
-            'data_gejala'   => $data_gejala
-        ];
+                                    if($cf1 < 0 || $cf2 < 0) {
+                                        $cf_combine = ($cf1 + $cf2) / (1 - min($cf1, $cf2));
+                                    } else {
+                                        $cf_combine = $cf1 + ($cf2 * (1 - $cf1));
+                                    }
+
+                                    $hasil_cf = $cf_combine;
+                                }
+                            }
+
+                            if(count($final) - 1 == $key) {
+                                if($cf_value == null) {
+                                    $cf_value = [$hasil_cf, "{$final[0]->kd_diagnosa} ({$final[0]->kd_gejala})"];
+                                } else {
+                                    $cf_value = ($hasil_cf > $cf_value[0])
+                                        ? [$hasil_cf, "{$final[0]->kd_diagnosa} ({$final[0]->kd_gejala})"]
+                                        : $cf_value;
+                                }
+
+                                $hasil_diagnosa[$final[0]->kd_diagnosa]['hasil_cf'] = $hasil_cf * 100;
+
+                                $cf1 = null;
+                                $cf2 = null;
+                                $cf_combine = 0;
+                                $hasil_cf = null;
+                            }
+
+                            if(empty($hasil_diagnosa[$final[0]->kd_diagnosa])) {
+                                    $hasil_diagnosa[$final[0]->kd_diagnosa] = [
+                                        // 'nama_penyakit' => $final[0]->kd_penyakit,
+                                        'kode_penyakit' => $final[0]->kd_diagnosa,
+                                        'hasil_cf' => $hasil_cf * 100
+                                    ];
+                            }
+                        }
+                        // dd(count($hasil_diagnosa));
+                        if(count($hasil_diagnosa) == 1) {
+                            if($hasil_diagnosa[$final[0]->kd_diagnosa]) {
+                                $data_diagnosa = $hasil_diagnosa[$final[0]->kd_diagnosa]['kode_penyakit'];
+                                $nilai_hasil = $hasil_diagnosa[$final[0]->kd_diagnosa]['hasil_cf'];
+                                $data_diagnosa2 = '-';
+                                $nilai_hasil2 = '0';
+                            }
+
+                            // else {
+                            //     $data_diagnosa = $hasil_diagnosa['P02']['kode_penyakit'];
+                            //     $nilai_hasil = $hasil_diagnosa['P02']['hasil_cf'];
+                            //     $data_diagnosa2 = '-';
+                            //     $nilai_hasil2 = '0';
+                            // }
+                        }
+
+                    }
+                }
+            }
+
+            // dd([$data_diagnosa, $nilai_hasil, $data_diagnosa2, $nilai_hasil2]);die;
+
+            return [
+                'data_diagnosa' => $data_diagnosa,
+                'nilai_hasil'   => $nilai_hasil,
+                'data_diagnosa2' => $data_diagnosa2,
+                'nilai_hasil2'   => $nilai_hasil2,
+                // 'data_gejala'   => $data_gejala
+            ];
+        //}
+
     }
 
 
@@ -214,7 +276,48 @@ class PagesController extends Controller
         ]);
 
         $data = $request->all();
-        $get_data = $this->perhitungan_diagnosa($data);
+        $jml_gjl_dipilih = 0;
+        foreach($data['bobot_gejala'] as $value) {
+            if(!empty($value)) {
+                $jml_gjl_dipilih += 1;
+                $opts = explode('+', $value);
+                $data_gejala[] = $opts[0];
+                $gejala = DB::table('knowladges')->where('kd_gejala' ,$opts[0])->get();
+
+                foreach($gejala as $valuegejala) {
+                    if(empty($data_penyakit[$valuegejala->kd_diagnosa])) {
+                        $data_penyakit[$valuegejala->kd_diagnosa] = [$valuegejala, [$valuegejala, $opts[1], $valuegejala->nilai_mb, $valuegejala->kd_diagnosa, $valuegejala->kd_gejala]];
+                    } else {
+                        array_push($data_penyakit[$valuegejala->kd_diagnosa], [$valuegejala, $opts[1], $valuegejala->nilai_mb, $valuegejala->kd_diagnosa, $valuegejala->kd_gejala]);
+                    }
+                }
+            }
+        }
+
+        // dd($data_penyakit);die;
+        if($jml_gjl_dipilih <= 0) {
+            Alert::warning('Gagal', 'Anda belum memilih gejala!');
+            return redirect('sp-konsultasi');
+        }
+
+        if($jml_gjl_dipilih < 2) {
+            Alert::warning('Gagal', 'Anda harus memilih lebih dari satu gejala!');
+            return redirect('sp-konsultasi');
+        }
+
+        $jml_p01 = count($data_penyakit['P01']);
+        $jml_p02 = count($data_penyakit['P02']);
+
+        $get_data = $this->perhitungan_diagnosa($data_penyakit);
+        // if($jml_p01 >= 3 && $jml_p02 >= 3) {
+        //     $get_data = $this->perhitungan_diagnosa($data_penyakit);
+        // } else if($jml_p01 >= 3) {
+        //     $get_data = $this->perhitungan_diagnosa($data_penyakit[]);
+        // } else if($jml_p02 >= 3) {
+        //     $get_data = $this->perhitungan_diagnosa($data_penyakit['P02']);
+        // }
+
+
 
         $kunjungan = [
             'nama'          => $request->nama,
@@ -230,7 +333,7 @@ class PagesController extends Controller
                             ->select('id')
                             ->orderBy('id', 'DESC')
                             ->first();
-        foreach($get_data['data_gejala'] as $value) {
+        foreach($data_gejala as $value) {
             $detail_kunjungan = [
                 'id_kunjungan' => $id_kunjungan->id,
                 'id_gejala'    => $value
